c97ec11889dfb187271293cdbfc1abedcc80aaa3
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-11-11 22:23:19 +0300

    Выполнение правок по видеоуроку #12

---
+++ script/main.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", function() {
+  "use strict";
+
+  // === Timer ===
+  function countTimer(deadline) {
+    console.log('deadline: ', deadline);
+    const timerHours = document.querySelector("#timer-hours"),
+          timerMinutes = document.querySelector("#timer-minutes"),
+          timerSeconds = document.querySelector("#timer-seconds");
+
+    function getTimeRemaining() {
+      const dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining) % 60,
+            minutes = Math.floor(timeRemaining / 60) % 60,
+            hours = Math.floor(timeRemaining / 3600);
+      return {
+        timeRemaining,
+        hours,
+        minutes,
+        seconds,
+      };
+    }
+
+    function updateClock() {
+      const timer = getTimeRemaining();
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+      if (timer.timeRemaining > 0) {
+        setTimeout(updateClock, 1000);
+      }
+    }
+
+    updateClock();
+  }
+
+  // countTimer("12 november 2019");
+  let date = new Date();
+  date.setDate(date.getDate() + 1);
+  countTimer(date.toDateString());
+  // === END OF === Timer ===
+
+
+});
