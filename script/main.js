82a4f6ac6e6bec03dda752cce8dafee7548b16e7
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-11-17 20:18:32 +0300

    Доработка замены фото участника команды

--- script/main.js
+++ script/main.js
@@ -339,19 +339,19 @@ window.addEventListener("DOMContentLoaded", () => {
   const switchPhoto = () => {
     const command = document.getElementById("command"),
           commandRow = command.querySelector(".row"),
-          photos = document.querySelectorAll(".command__photo");
+          photos = [...(document.querySelectorAll(".command__photo"))];
 
     // События прохода курсора над фото
     commandRow.addEventListener("mouseover", (event) => {
       const { target } = event;
-      if ([...photos].indexOf(target) >= 0) {
+      if (photos.indexOf(target) >= 0) {
         target.dataset.defaultImg = target.getAttribute("src");
         target.setAttribute("src", target.dataset.img);
       }
     });
     commandRow.addEventListener("mouseout", (event) => {
       const { target } = event;
-      if ([...photos].indexOf(target) >= 0) {
+      if (photos.indexOf(target) >= 0) {
         target.setAttribute("src", target.dataset.defaultImg);
       }
     });
