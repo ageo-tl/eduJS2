d39eb56fcfc1a2736ff7cef4f7093ca103028e52
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-11-17 11:51:37 +0300

    Закрытие меню при клике вне его пределов

--- script/main.js
+++ script/main.js
@@ -56,6 +56,9 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const handlerMenu = (event) => {
       const { target } = event;
+      if (menu.classList.contains("active-menu") && !target.closest("menu")) {
+        menu.classList.remove("active-menu");
+      }
       if (target.closest(".menu, .close-btn, menu>ul>li")) {
         menu.classList.toggle("active-menu");
       }
