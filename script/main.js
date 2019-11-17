dbdfb330d604d44e27840fba02aa774cc1bd1559
Anton Ivanov <geo.alpha.contact@gmail.com>
2019-11-17 20:08:26 +0300

    Замена фото участника команды при наведении мыши

--- script/main.js
+++ script/main.js
@@ -334,4 +334,29 @@ window.addEventListener("DOMContentLoaded", () => {
   playSlider();
   // ===END OF === Slider ===
 
+
+  // === Command Photo ===
+  const switchPhoto = () => {
+    const command = document.getElementById("command"),
+          commandRow = command.querySelector(".row"),
+          photos = document.querySelectorAll(".command__photo");
+
+    // События прохода курсора над фото
+    commandRow.addEventListener("mouseover", (event) => {
+      const { target } = event;
+      if ([...photos].indexOf(target) >= 0) {
+        target.dataset.defaultImg = target.getAttribute("src");
+        target.setAttribute("src", target.dataset.img);
+      }
+    });
+    commandRow.addEventListener("mouseout", (event) => {
+      const { target } = event;
+      if ([...photos].indexOf(target) >= 0) {
+        target.setAttribute("src", target.dataset.defaultImg);
+      }
+    });
+  };
+  switchPhoto();
+  // === END OF === Command Photo ===
+
 });
